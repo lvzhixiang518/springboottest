ee022bd726191d23d77e585eae674c991955f50b
lvzhixiang <962745314@qq.com>
2020-09-03 15:25:31 +0800

    即将准备合并 2020年9月3日15:25:23 add by lzx

--- src/main/java/com/Java/Java8test.java
+++ src/main/java/com/Java/Java8test.java
@@ -1,13 +1,5 @@
 package com.Java;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-
-import java.sql.Timestamp;
-import java.util.*;
-import java.util.stream.Collectors;
-
 /**
  * @author lvzhixiang
  * @since 2020/8/28
@@ -15,11 +7,15 @@ import java.util.stream.Collectors;
 public class Java8test {
 
     public static void main(String[] args) {
-        /*List<String> strList = Arrays.asList(new String[]{"zhangsan", "lisi", "wangwu", "maliu"});
+        System.out.println(System.currentTimeMillis());
+    }
+
+    /*public static void main(String[] args) {
+        *//*List<String> strList = Arrays.asList(new String[]{"zhangsan", "lisi", "wangwu", "maliu"});
         List<Integer> intList = Arrays.asList(new Integer[]{1, null, 3,3, null, 4, null, 5});
         //intList.forEach(System.out::println);
         List<Integer> tt = intList.stream().distinct().filter(num->num!=null).limit(2).collect(Collectors.toList());
-        tt.forEach(System.out::println);*/
+        tt.forEach(System.out::println);*//*
         HttpHeaders httpHeaders = new HttpHeaders();
         MediaType type = MediaType.parseMediaType("application/json;charset=UTF-8");
         httpHeaders.setContentType(type);
@@ -32,5 +28,5 @@ public class Java8test {
         System.out.println(str);
         Map map = new HashMap();
         map.putAll(new HashMap());
-    }
+    }*/
 }
